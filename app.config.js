bfd8b42b2ac61d58f24765cf72484d031b627841
Lucas Lima <lucaslavey@outlook.com>
2024-05-14 21:16:51 -0300

    add configure build

--- app.config.js
+++ app.config.js
@@ -2,8 +2,9 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "Des-pensa",
-    slug: "Des-pensa",
+    owner: "des-pensa",
+    name: "des-pensa",
+    slug: "des-pensa",
     privacy: "public",
     platforms: ["ios", "android"],
     version: "0.0.1",
@@ -28,6 +29,18 @@ export default {
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
+      eas: {
+        projectId: "2d14742c-ed24-428d-bf17-28128acbd7ff",
+      },
+    },
+    android: {
+      package: "com.seuapp", // Substitua com o pacote correto do seu app
+    },
+    updates: {
+      url: "https://u.expo.dev/2d14742c-ed24-428d-bf17-28128acbd7ff",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
     },
   },
 };
